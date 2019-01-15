c7523eb148e0be4118ab61bce5c98bcc1be4c232
Ian Rocha <ianbritosr@gmail.com>
2019-01-15 18:24:43 -0300

    Update settings, update home template/view, create base html/css/js

--- ordermaker/views.py
+++ ordermaker/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 
-class HomePage(View):
-    def get(self, request):
-        return HttpResponse('Hello World!')
+class HomePage(TemplateView):
+    template_name = 'home.html'
