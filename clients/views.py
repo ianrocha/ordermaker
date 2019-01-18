f27b2760747ea3dd4b9ec083b80d6c8e2352c228
Ian Rocha <ianbritosr@gmail.com>
2019-01-18 11:29:12 -0300

    Update clients and products list view

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 
-from carts.models import Cart, CartItem
+from carts.models import Cart
 from .models import Client
 
 
@@ -10,9 +10,7 @@ class ClientListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super(ClientListView, self).get_context_data(*args, **kwargs)
         cart_obj, new_obj = Cart.objects.new_or_get(self.request)
-        cart_items = CartItem.objects.all().filter(cart=cart_obj)
         context['cart'] = cart_obj
-        context['cart-items'] = cart_items
         return context
 
     def get_queryset(self, *args, **kwargs):
--- products/views.py
+++ products/views.py
@@ -1,13 +1,19 @@
 from django.views.generic import ListView
 
+from carts.models import Cart, CartItem
 from .models import Product
 
 
 class ProductListView(ListView):
     template_name = 'products/list.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data()
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        cart_items = CartItem.objects.all().filter(cart__exact=cart_obj)
+        cart_items_obj = [item.product for item in cart_items]
+        context['cart'] = cart_obj
+        context['cart_items'] = cart_items_obj
         return context
 
     def get_queryset(self, *args, **kwargs):
