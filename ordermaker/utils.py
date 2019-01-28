136728fb8f9a1cf1e956b3d7b9c4bf33e72f6a2b
Ian Rocha <ianbritosr@gmail.com>
2019-01-28 17:10:22 -0300

    Add docstring for utils file

--- ordermaker/utils.py
+++ ordermaker/utils.py
@@ -3,12 +3,24 @@ import random
 
 
 def get_filename_ext(filepath):
+    """
+    Receive a filepath; get and separate the original file name
+    from the extension and return both
+    :param filepath: the directory from where the file is inputted
+    :return: the original file name and the extension
+    """
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
     return name, ext
 
 
 def upload_image_path(filename, model):
+    """
+    Receive a file name and a model name and return a new name with the local of the file
+    :param filename: Name of the file
+    :param model: Name of the model
+    :return: New name and local of the file
+    """
     new_filename = random.randint(1, 39010209312)
     name, ext = get_filename_ext(filename)
     final_filename = '{}{}'.format(new_filename, ext)
@@ -19,6 +31,12 @@ def upload_image_path(filename, model):
 
 
 def validate_quantity(quantity, default_quantity):
+    """
+    Verifies if 'quantity' is multiple of 'default_quantity
+    :param quantity: Value inputted by user
+    :param default_quantity: Default value of the system
+    :return: True or False
+    """
     result = quantity % default_quantity
     if result != 0:
         return False
@@ -26,6 +44,11 @@ def validate_quantity(quantity, default_quantity):
 
 
 def validate_profitability(profitability):
+    """
+    Verifies if 'profitability' is 'Bad'
+    :param profitability: Value inputted by user
+    :return: True or False
+    """
     if profitability == 'Bad':
         return False
     return True
